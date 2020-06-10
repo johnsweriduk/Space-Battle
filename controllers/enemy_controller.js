59ba0adfdce2a705a28c475c9de2e211c7e8c1be
John Sweriduk <john@stackandsizzle.com>
2020-06-09 22:31:26 -0700

    updated wave logic

--- controllers/enemy_controller.js
+++ controllers/enemy_controller.js
@@ -4,17 +4,10 @@ const router = express.Router();
 router.get('/:level', (req, res) => {
     const level = req.params.level;
     const waves = [];
-    let numWaves = 3;
-    if(level > 5) {
-        numWaves = 5;
-        if(level > 8) {
-            numWaves = 7;
-        }
-    }
-    numwaves = 2 + level;
+    let numWaves = 2 + level;
     const wave  = [];
     for(let i = 1; i <= numWaves; i++) {
-        wave.push(Math.ceil(i / 2));
+        wave.push(Math.ceil(i / 2) + Math.floor(level / 3));
     }
     res.send(wave);
 });
